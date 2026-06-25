37a6e363710a9bf74b2a21879c4a675431af2daf
hatim <hatimmnomani@outlook.com>
2026-06-25 14:03:55 +0530

    feat(hijri-core): locale data and format/parse

---
+++ packages/hijri-core/src/locale.ts
@@ -0,0 +1,39 @@
+export const translitMonthNames: string[] = [
+  "Moharram al-Haraam",
+  "Safar al-Muzaffar",
+  "Rabi al-Awwal",
+  "Rabi al-Aakhar",
+  "Jumada al-Ula",
+  "Jumada al-Ukhra",
+  "Rajab al-Asab",
+  "Shaban al-Karim",
+  "Ramadan al-Moazzam",
+  "Shawwal al-Mukarram",
+  "Zilqadah al-Haraam",
+  "Zilhaj al-Haraam",
+];
+
+export const arMonthNames: string[] = [
+  "محرم الحرام",
+  "صفر المظفر",
+  "ربيع الأول",
+  "ربيع الآخر",
+  "جمادى الأولى",
+  "جمادى الأخرى",
+  "رجب الأصب",
+  "شعبان الكريم",
+  "رمضان المعظم",
+  "شوال المكرم",
+  "ذو القعدة الحرام",
+  "ذو الحجة الحرام",
+];
+
+export const weekdayNames: string[] = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
